13358fbaab7cb899da7b0c88995556348ab1c30f
cotton615 <egorsever07@gmail.com>
2025-03-27 21:26:20 +0200

    Jump and Interpolation Searches

--- CSharpTasks/Program.cs
+++ CSharpTasks/Program.cs
@@ -1,83 +1,74 @@
-﻿namespace CSharpTasks
-{
-    internal class Program
+﻿namespace CSharpTasks {
+    internal class Program 
     {
-      /*Задание: Реализация метода TryGetValue для массива
-        Реализуйте метод TryGetValue, который выполняет линейный поиск элемента в массиве целых чисел.
-
-        Метод должен соответствовать следующим требованиям:
-            • Принимает на вход массив целых чисел и искомое число.
-            • Если элемент найден, метод возвращает true и инициализирует out-параметр индексом найденного элемента.
-            • Если элемент не найден, метод возвращает false, а out-параметр получает значение -1.
-            • Поиск должен выполняться линейно (без встроенных методов поиска).
-            • Задокументировать метод summary комментарием.*/
-
-        /// <summary>
-        ///  Finds the element in the array.
-        /// </summary>
-        /// <param name="arr">Array, in which seeks.</param>
-        /// <param name="seek">Element, which it seeks.</param>
-        /// <param name="index">The index, which will be returned.</param>
-        /// <returns>Index in case if element is found and returns -1 if element is not found.</returns>
-        static bool TryGetValue(int[] arr, int seek, out int index) {
-            for (int i = 0; i < arr.Length; i++) {
-
-                if (arr[i] == seek) {
-                    index = i;
-                    Console.WriteLine($"Element {seek} is found.");
-                    return true;
-                } 
+        static int InterpolationSearch(int[] arr, int seek, int left, int right) {
+            if (left == right || arr[left] == arr[right]) {
+                throw new ArgumentException($"Element {seek} is not found");
+            } 
+            
+            int pos = left + ((seek - arr[left]) * (right - left)) / (arr[right] - arr[left]);
+            if (arr[pos] == seek) {
+                return pos;
+            } else {
+                if (arr[pos] < seek) {
+                    left = pos + 1;
+                } else if (arr[pos] > seek) {
+                    right = pos - 1;
+                }
             }
-            Console.WriteLine($"Element {seek} isn't found in the given array");
-            index = -1;
-            return false;
+            
+            return InterpolationSearch(arr, seek, left, right);
         }
 
+        static int JumpSearch(int[] arr, int seek) {
+            int step = (int)Math.Sqrt(arr.Length);
 
-      /*Задание: Реализация метода FindIndex для массива
+            for (int i = 0; i < arr.Length; i += step) {
 
-        Реализуйте метод FindIndex, который выполняет линейный поиск элемента в массиве целых чисел.
-
-        Метод должен соответствовать следующим требованиям:
-            • Принимает на вход массив целых чисел и искомое число.
-            • Если элемент найден, метод возвращает его индекс.
-            • Если элемент не найден, метод выбрасывает исключение InvalidOperationException с сообщением "Элемент не найден в массиве".
-            • Поиск должен выполняться линейно (без встроенных методов поиска).
-            • Метод должен быть задокументирован с помощью summary-комментария.*/
-
-        /// <summary>
-        /// Finds index of the sought element.
-        /// </summary>
-        /// <param name="arr">Array, in which seek.s</param>
-        /// <param name="seek">Element, index of which it must find.</param>
-        /// <returns>Index of the sought element.</returns>
-        /// <exception cref="InvalidOperationException">Throws exception, if element is not found.</exception>
-        static int FindIndex(int[] arr, int seek) {
-            for (int i = 0; i < arr.Length; i++) {
-                if (arr[i] == seek) {
-                    return i;
-                } 
+                if (arr[i] < seek) {
+                    continue;    
+                } else {
+                    for (int j = i - step; j < i; j++) {
+                        if (arr[j] == seek) {
+                            return j;
+                        }
+                    }
+                }
             }
-            throw new InvalidOperationException($"Element {seek} isn't found in the given array.");
+            throw new ArgumentException("Element is not found");
         }
 
-        static void Main()
-        {
-            int[] arr = new int[5];
+        private static void Main() {
+            int[] arr = new int[10];
+
             for (int i = 0; i < arr.Length; i++) {
                 arr[i] = Random.Shared.Next(0, 11);
+            }
+
+            Array.Sort(arr);
+            for (int i = 0; i < arr.Length; i++) {
                 Console.Write($"{arr[i]} ");
             }
             Console.WriteLine();
 
+            int index;
+            int seek = 5;
             try {
-                int result = FindIndex(arr, 5);
-                Console.WriteLine($"Index of the sought element: {result}.");
-            } catch (InvalidOperationException e) {
-                Console.WriteLine(e.Message);
+                index = JumpSearch(arr, seek);
+            } catch (Exception ex) {
+                Console.WriteLine($"{ex.Message}");
+                return;
             }
 
-            TryGetValue(arr, 5, out int index);
+            Console.WriteLine($"Jump search:\nElement is found: {index}\n");
+
+            try {
+                index = InterpolationSearch(arr, seek, 0, arr.Length - 1);
+            } catch (Exception ex) {
+                Console.WriteLine($"{ex.Message}");
+                return;
+            }
+            Console.WriteLine($"Interpolation search:\nElement is found: {index}");
         }
     }
 }
