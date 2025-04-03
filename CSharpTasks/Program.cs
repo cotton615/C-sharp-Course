812717935c2ea5b390e16c861bee32649610cd65
cotton615 <egorsever07@gmail.com>
2025-04-03 21:03:06 +0300

    Sorting Algorithms

--- CSharpTasks/Program.cs
+++ CSharpTasks/Program.cs
@@ -1,74 +1,119 @@
 ﻿namespace CSharpTasks {
-    internal class Program 
-    {
-        static int InterpolationSearch(int[] arr, int seek, int left, int right) {
-            if (left == right || arr[left] == arr[right]) {
-                throw new ArgumentException($"Element {seek} is not found");
-            } 
-            
-            int pos = left + ((seek - arr[left]) * (right - left)) / (arr[right] - arr[left]);
-            if (arr[pos] == seek) {
-                return pos;
-            } else {
-                if (arr[pos] < seek) {
-                    left = pos + 1;
-                } else if (arr[pos] > seek) {
-                    right = pos - 1;
+
+    internal class Program {
+
+        static void ShellSort(int[] arr) {
+            int d = arr.Length / 2;
+            while (d > 0) { 
+                for (int i = d; i < arr.Length; i++) {
+
+                    int j = i;
+                    int temp = arr[i];
+                    while (j >= d && arr[j-d] > temp) {
+                        arr[j] = arr[j - d];
+                        j--;
+                    }
+                    arr[j] = temp;
+
                 }
+            d /= 2;
             }
-            
-            return InterpolationSearch(arr, seek, left, right);
         }
 
-        static int JumpSearch(int[] arr, int seek) {
-            int step = (int)Math.Sqrt(arr.Length);
+        static void CocktailSort(int[] arr, int start, int end) {
+            while (start < end) {
+                for (int i = start; i < end; i++) {
 
-            for (int i = 0; i < arr.Length; i += step) {
+                    if (arr[i] > arr[i + 1]) {
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                    }
 
-                if (arr[i] < seek) {
-                    continue;    
-                } else {
-                    for (int j = i - step; j < i; j++) {
-                        if (arr[j] == seek) {
-                            return j;
+                    for (int j = end; j > start; j--) {
+                        if (arr[j] < arr[j - 1]) {
+                            int temp = arr[j];
+                            arr[j] = arr[j - 1];
+                            arr[j - 1] = temp;
                         }
                     }
                 }
+                start += 1;
+                end -= 1;
             }
-            throw new ArgumentException("Element is not found");
+        }
+
+        static void InsertionSort(int[] arr) {
+            for (int i = 1; i < arr.Length; i++) {
+                int key = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > key) {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j+1] = key;
+            }
+        }
+        
+        static void SelectionSort(int[] arr, int start) {
+            if (start == arr.Length) {
+                return;
+            }
+
+            int minIndex = start;
+            for (int i = start; i < arr.Length-1; i++) {
+                if (arr[minIndex] > arr[i+1]) {
+                    minIndex = i+1;
+                } else if (arr[minIndex] < arr[i+1]){
+                    continue;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[start]; 
+            arr[start] = temp;
+            SelectionSort(arr, start+1);
         }
 
         private static void Main() {
             int[] arr = new int[10];
-
             for (int i = 0; i < arr.Length; i++) {
-                arr[i] = Random.Shared.Next(0, 11);
+                arr[i] = Random.Shared.Next(11);
             }
 
-            Array.Sort(arr);
+            Console.WriteLine("Default array: ");
             for (int i = 0; i < arr.Length; i++) {
                 Console.Write($"{arr[i]} ");
             }
-            Console.WriteLine();
+            Console.WriteLine("\n\n");
 
-            int index;
-            int seek = 5;
-            try {
-                index = JumpSearch(arr, seek);
-            } catch (Exception ex) {
-                Console.WriteLine($"{ex.Message}");
-                return;
+            SelectionSort(arr, 0);
+            Console.WriteLine("Selection sorted array: ");
+            for (int i = 0; i < arr.Length; i++) {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine("\n\n");
+
+            InsertionSort(arr);
+            Console.WriteLine("Insertion sorted array: ");
+            for (int i = 0; i < arr.Length; i++) {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine("\n\n");
+
+            ShellSort(arr);
+            Console.WriteLine("Shell Sorted Array: ");
+            for (int i = 0; i < arr.Length; i++) {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine("\n\n");
+
+            CocktailSort(arr, 0, arr.Length - 1);
+            Console.WriteLine("Cocktail Sorted Array: ");
+            for (int i = 0; i < arr.Length; i++) {
+                Console.Write($"{arr[i]} ");
             }
 
-            Console.WriteLine($"Jump search:\nElement is found: {index}\n");
-
-            try {
-                index = InterpolationSearch(arr, seek, 0, arr.Length - 1);
-            } catch (Exception ex) {
-                Console.WriteLine($"{ex.Message}");
-                return;
-            }
-            Console.WriteLine($"Interpolation search:\nElement is found: {index}");
         }
     }
 }
