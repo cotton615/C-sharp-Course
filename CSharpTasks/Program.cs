2e7d00e816efd7957134b0ea828a0f8617892fc8
cotton615 <egorsever07@gmail.com>
2025-04-10 20:43:52 +0300

    MergeSort with QuickSort

--- CSharpTasks/Program.cs
+++ CSharpTasks/Program.cs
@@ -1,119 +1,93 @@
-﻿namespace CSharpTasks {
+﻿using System.Security.Cryptography;
+
+namespace CSharpTasks {
 
     internal class Program {
-
-        static void ShellSort(int[] arr) {
-            int d = arr.Length / 2;
-            while (d > 0) { 
-                for (int i = d; i < arr.Length; i++) {
-
-                    int j = i;
-                    int temp = arr[i];
-                    while (j >= d && arr[j-d] > temp) {
-                        arr[j] = arr[j - d];
-                        j--;
-                    }
-                    arr[j] = temp;
-
+        static int Partition(int[] array, int start, int end) {
+            int marker = start; 
+            for (int i = start; i < end; i++) {
+                if (array[i] < array[end]) 
+                {
+                    (array[marker], array[i]) = (array[i], array[marker]);
+                    marker += 1;
                 }
-            d /= 2;
             }
+            (array[marker], array[end]) = (array[end], array[marker]);
+            return marker;
         }
 
-        static void CocktailSort(int[] arr, int start, int end) {
-            while (start < end) {
-                for (int i = start; i < end; i++) {
-
-                    if (arr[i] > arr[i + 1]) {
-                        int temp = arr[i];
-                        arr[i] = arr[i + 1];
-                        arr[i + 1] = temp;
-                    }
-
-                    for (int j = end; j > start; j--) {
-                        if (arr[j] < arr[j - 1]) {
-                            int temp = arr[j];
-                            arr[j] = arr[j - 1];
-                            arr[j - 1] = temp;
-                        }
-                    }
-                }
-                start += 1;
-                end -= 1;
-            }
-        }
-
-        static void InsertionSort(int[] arr) {
-            for (int i = 1; i < arr.Length; i++) {
-                int key = arr[i];
-                int j = i - 1;
-                while (j >= 0 && arr[j] > key) {
-                    arr[j + 1] = arr[j];
-                    j--;
-                }
-                arr[j+1] = key;
-            }
-        }
-        
-        static void SelectionSort(int[] arr, int start) {
-            if (start == arr.Length) {
+        static void QuickSort(int[] array, int start, int end) {
+            if (start >= end)
                 return;
+
+            int pivot = Partition(array, start, end);
+            QuickSort(array, start, pivot - 1);
+            QuickSort(array, pivot + 1, end);
+        }
+
+        static int[] MergeSort(int[] arr, int threshold) {
+            if (arr.Length <= threshold) {
+                QuickSort(arr, 0, arr.Length-1);
+                return arr;
             }
 
-            int minIndex = start;
-            for (int i = start; i < arr.Length-1; i++) {
-                if (arr[minIndex] > arr[i+1]) {
-                    minIndex = i+1;
-                } else if (arr[minIndex] < arr[i+1]){
-                    continue;
+            int mid = arr.Length / 2;
+
+            int[] leftArray = new int[mid];
+            int[] rightArray = new int[arr.Length-mid];
+
+            Array.Copy(arr, 0, leftArray, 0, mid);
+            Array.Copy(arr, mid, rightArray, 0, arr.Length-mid);
+
+            return Merge(MergeSort(leftArray, threshold), MergeSort(rightArray, threshold));
+        }
+
+        static int[] Merge(int[] leftArr, int[] rightArr) {
+            int leftPointer = 0;
+            int rightPointer = 0;
+
+            int[] merged = new int[leftArr.Length + rightArr.Length];
+
+            for (int i = 0; i < merged.Length; i++) {
+
+                if (leftPointer < leftArr.Length && rightPointer < rightArr.Length) {
+
+                    if (leftArr[leftPointer] > rightArr[rightPointer]) {
+                        merged[i] = rightArr[rightPointer];
+                        rightPointer++;
+                    } else {
+                        merged[i] = leftArr[leftPointer];
+                        leftPointer++;
+                    }
+                } else if (leftPointer < leftArr.Length){
+                    merged[i] = leftArr[leftPointer];
+                    leftPointer++;
+                } else if (rightPointer < rightArr.Length) {
+                    merged[i] = rightArr[rightPointer];
+                    rightPointer++;
                 }
             }
 
-            int temp = arr[minIndex];
-            arr[minIndex] = arr[start]; 
-            arr[start] = temp;
-            SelectionSort(arr, start+1);
+            return merged;
         }
 
-        private static void Main() {
-            int[] arr = new int[10];
+        static void Main() {
+            int[] arr = new int[100];
             for (int i = 0; i < arr.Length; i++) {
-                arr[i] = Random.Shared.Next(11);
+                arr[i] = Random.Shared.Next(0, 21);
             }
 
             Console.WriteLine("Default array: ");
             for (int i = 0; i < arr.Length; i++) {
                 Console.Write($"{arr[i]} ");
             }
-            Console.WriteLine("\n\n");
 
-            SelectionSort(arr, 0);
-            Console.WriteLine("Selection sorted array: ");
+            int threshold = arr.Length / 2; // threshold, to which MergeSort divides array
+            arr = MergeSort(arr, threshold);
+            Console.WriteLine("\n\nMerge+Quick Sorted array: ");
             for (int i = 0; i < arr.Length; i++) {
                 Console.Write($"{arr[i]} ");
             }
-            Console.WriteLine("\n\n");
-
-            InsertionSort(arr);
-            Console.WriteLine("Insertion sorted array: ");
-            for (int i = 0; i < arr.Length; i++) {
-                Console.Write($"{arr[i]} ");
-            }
-            Console.WriteLine("\n\n");
-
-            ShellSort(arr);
-            Console.WriteLine("Shell Sorted Array: ");
-            for (int i = 0; i < arr.Length; i++) {
-                Console.Write($"{arr[i]} ");
-            }
-            Console.WriteLine("\n\n");
-
-            CocktailSort(arr, 0, arr.Length - 1);
-            Console.WriteLine("Cocktail Sorted Array: ");
-            for (int i = 0; i < arr.Length; i++) {
-                Console.Write($"{arr[i]} ");
-            }
-
         }
     }
 }
