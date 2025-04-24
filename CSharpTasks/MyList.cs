0e94c97c68a573454a48a66f3908cd0cc4eb6244
cotton615 <egorsever07@gmail.com>
2025-04-24 22:32:37 +0300

    Classes. Operator overloading.

---
+++ CSharpTasks/MyList.cs
@@ -0,0 +1,61 @@
+﻿namespace CSharpTasks {
+    public class MyList {
+        private int[] _items;
+
+
+        // Getters and Setters
+        public int Length => _items.Length;
+        public int[] Items => _items;
+
+        public int this[int index] {
+            get => _items[index];
+            set => _items[index] = value;
+        }
+
+        // Constructors
+        public MyList(int length) {
+            _items = new int[length];
+        }
+
+        public MyList() {
+            _items = [];
+        }
+
+        public static MyList operator +(MyList firstArray, MyList secondArray) {
+            MyList resultArray = new MyList(firstArray.Length + secondArray.Length);
+
+            Array.Copy(firstArray.Items, resultArray._items, firstArray.Length);
+            Array.Copy(secondArray.Items, 0, resultArray._items, firstArray.Length, secondArray.Length);
+
+            return resultArray;
+        }
+
+
+        public static bool operator !=(MyList firstArray, MyList secondArray) {
+            return !(firstArray == secondArray);
+        }
+
+        public static bool operator ==(MyList firstArray, MyList secondArray) {
+            if (ReferenceEquals(firstArray, secondArray)) {
+                return true;
+            }
+
+            if (firstArray is null || secondArray is null) {
+                return false;
+            }
+
+
+            if (firstArray.Length != secondArray.Length) {
+                return false;
+            } else {
+                for (int i = 0; i < firstArray.Length; i++) {
+                    if (firstArray[i] != secondArray[i]) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- CSharpTasks/Program.cs
+++ CSharpTasks/Program.cs
@@ -1,81 +1,34 @@
-﻿
-namespace CSharpTasks {
+﻿namespace CSharpTasks {
 
     internal class Program {
         
-        static void RadixSort(int[] arr) {
-            if (arr.Length <= 1) {
-                return;
+       static void Main() {
+            MyList myList1 = new MyList(3);
+
+            for (int i = 0; i < myList1.Length; i++) {
+                myList1[i] = Random.Shared.Next(0, 10);
+            }
+            Console.WriteLine("First MyList:");
+            for (int i = 0; i < myList1.Length; i++) {
+                Console.Write($"{myList1[i]} ");
             }
 
-            int maxValue = FindMaxValue(arr);
+            MyList myList2 = new MyList(7);
 
-            for (int digitPlace = 1; digitPlace <= maxValue; digitPlace *= 10) {
-
-                int[,] buckets = new int[10, arr.Length]; // Create a 2D array to hold the buckets for each digit (0-9)
-                int[] bucketCount = new int[10]; // Array to count the number of elements in each bucket
-
-                for (int i = 0; i < arr.Length; i++) {  // placing elements in their buckets
-                    int digit = DigitFromNumber(arr[i], digitPlace);
-                    int bucketIndex = bucketCount[digit];
-
-                    buckets[digit, bucketIndex] = arr[i];
-                    bucketCount[digit]++;
-                }
-
-                int index = 0;
-                for (int i = 0; i < 10; i++) { // rearranging the array using the buckets
-                    for (int j = 0; j < bucketCount[i]; j++) {
-                        arr[index] = buckets[i, j];
-                        index++;
-                    }
-                }
-                
+            for (int i = 0; i < myList2.Length; i++) {
+                myList2[i] = Random.Shared.Next(0, 10);
             }
-        }
-
-        /// <summary>
-        /// Finds maximum value in the given array.
-        /// </summary>
-        /// <param name="arr">Array, in which seeks.</param>
-        /// <returns>Return maximum value from the array.</returns>
-        static int FindMaxValue(int[] arr) {
-            int maxDigit = 0;
-            for (int i = 0; i < arr.Length; i++) {
-                if (arr[i] > maxDigit) {
-                    maxDigit = arr[i];
-                }
-            }
-            return maxDigit;
-        }
-        /// <summary>
-        /// Extracts a digit from a number based on the specified place value.
-        /// </summary>
-        /// <param name="number">The number from which the digit is extracted.</param>
-        /// <param name="digitToFind">The place value (1 for ones, 10 for tens, 100 for hundreds, etc.).</param>
-        /// <returns>Returns the digit at the specified place value in the number.</returns>
-        static int DigitFromNumber(int number, int digitToFind) {
-            int digit = (number / digitToFind) % 10;
-
-            return digit;
-        }
-
-        static void Main() {
-            int[] arr = new int[10];
-            for (int i = 0; i < arr.Length; i++) {
-                arr[i] = Random.Shared.Next(0, 100);
+            Console.WriteLine("\nSecond MyList:");
+            for (int i = 0; i < myList2.Length; i++) {
+                Console.Write($"{myList2[i]} ");
             }
 
-            Console.WriteLine("Default array: ");
-            for (int i = 0; i < arr.Length; i++) {
-                Console.Write($"{arr[i]} ");
+            Console.WriteLine("\n\nAdded MyLists: ");
+            MyList result = myList1 + myList2;
+            for (int i = 0; i < result.Length; i++) {
+                Console.Write($"{result[i]} ");
             }
-            RadixSort(arr);
-
-            Console.WriteLine("\n\nRadix Sorted array: ");
-            for (int i = 0; i < arr.Length; i++) {
-                Console.Write($"{arr[i]} ");
-            }
+            Console.WriteLine($"\n\nTest of ==: {myList1 == myList2} and !=: {myList1 != myList2}");
         }
     }
 }
